04aed863e65f8c7df39a0a8404d78484d5234286
admin <love-nankai@163.com>
2016-10-14 16:18:07 +0800

    Extension of UIView to add handy method for shake.

---
+++ Source/UIView+Shake.swift
@@ -0,0 +1,52 @@
+//
+//  UIView+Shake.swift
+//  UIView+Shake
+//
+//  Created by admin on 2016/10/14.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import UIKit
+
+public enum ShakeDirection: Int {
+    case vertical, horizontal
+}
+
+public extension UIView {
+    public func shake(_ times: UInt = 10,
+                      offset: CGFloat = 5,
+                      speed: TimeInterval = 0.03,
+                      direction: ShakeDirection = .horizontal,
+                      completion: (()->())? = nil) {
+        p_shake(times, currentTimes: 0, flag: 1, offset: offset, speed: speed, direction: direction, completion: completion)
+    }
+    
+    private func p_shake(_ times: UInt,
+                         currentTimes: UInt,
+                         flag: Int,
+                         offset: CGFloat,
+                         speed: TimeInterval,
+                         direction: ShakeDirection,
+                         completion: (()->())?) {
+        
+        UIView.animate(withDuration: speed, animations: {
+            self.transform = direction == .horizontal ? CGAffineTransform(translationX: offset * CGFloat(flag), y: 0) : CGAffineTransform(translationX: 0, y: offset * CGFloat(flag))
+        }) { finished in
+            if currentTimes >= times {
+                UIView.animate(withDuration: speed, animations: {
+                    self.transform = CGAffineTransform.identity
+                    }, completion: { finished in
+                        completion?()
+                })
+                return
+            }
+            self.p_shake(times - 1,
+                         currentTimes: currentTimes + 1,
+                         flag: flag * (-1),
+                         offset: offset,
+                         speed: speed,
+                         direction: direction,
+                         completion: completion)
+        }
+    }
+}
---
+++ UIView+Shake/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  UIView+Shake
+//
+//  Created by admin on 2016/10/14.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var shakeView: UIView!
+    @IBOutlet weak var textTimes: UITextField!
+    @IBOutlet weak var textSpeed: UITextField!
+    @IBOutlet weak var textOffset: UITextField!
+    @IBOutlet weak var shakeDirection: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViews()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func initViews() {
+        for textFeild in [textOffset, textTimes, textSpeed] {
+            textFeild?.layer.borderWidth = 2
+            textFeild?.layer.borderColor = UIColor(red: 49.0/255.0, green: 186.0/255.0, blue: 81.0/255.0, alpha: 1.0).cgColor
+        }
+        shakeDirection.layer.borderWidth = 2
+        shakeDirection.layer.borderColor = shakeDirection.tintColor.cgColor
+        shakeDirection.selectedSegmentIndex = 1
+        
+        shakeView.layer.cornerRadius = 21
+        shakeView.layer.masksToBounds = true
+    }
+    
+    @IBAction func shake(_ sender: AnyObject) {
+        let direction: ShakeDirection = shakeDirection.selectedSegmentIndex == 0 ? .horizontal : .vertical
+        guard let times = UInt(textTimes.text!) else {
+            return
+        }
+        shakeView.shake(times, offset: CGFloat((textOffset.text! as NSString).floatValue), speed: TimeInterval((textSpeed.text! as NSString).floatValue), direction: direction)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+
+}
+
